9c055209e5073c8be737126b4c7fbf4b20f0b7ae
Coke Javier <lopezespinosjavier@gmail.com>
2021-09-20 21:19:15 +0200

    KYK-01: Creating Layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import React from 'react';
+
 function App() {
-  return (
-    <h1>
-      Know your kitten
-    </h1>
-  );
+    return (
+        <h1>Random Cat Stuff</h1>
+    );
 }
 
 export default App;
---
+++ src/components/App/App.tsx
@@ -0,0 +1,15 @@
+import Title from '../Title/Title'
+import Grid from '../Grid/Grid'
+import Button from '../Button/Button'
+
+function App() {
+    return (
+        <>
+            <Title />
+            <Grid />
+            <Button />
+        </>
+    );
+}
+
+export default App;
