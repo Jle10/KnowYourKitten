d540962e6b2192203773fc85aa2085641304f537
Coke Javier <lopezespinosjavier@gmail.com>
2021-09-24 01:20:56 +0200

    KYK-06 - Adding Unitary Test

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,9 +1,25 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from "@testing-library/user-event";
+import FactsLoading from "../../hooks/FactsLoading";
 
-test('renders and tests the app', () => {
+jest.mock('../../hooks/FactsLoading');
+const mockFactsLoading = FactsLoading as jest.MockedFunction<typeof FactsLoading>
+
+test('renders and tests the app', async () => {
     render(<App />);
     const mainContainer = screen.getByTestId('main-container')
     expect(mainContainer).toBeInTheDocument();
     expect(mainContainer.tagName).toBe('MAIN');
+
+    expect(mockFactsLoading).toBeCalledTimes(1);
+
+    const cards = await screen.queryAllByTestId('card-container');
+    expect(cards).toMatchSnapshot();
+
+    const button = screen.getByTestId('button');
+    expect(button).toBeInTheDocument();
+
+    userEvent.click(button);
+    expect(mockFactsLoading).toBeCalledTimes(2);
 });
--- src/components/Button/Button.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Button from './Button';
-
-test('renders and tests the app', () => {
-    render(<Button />);
-    const button = screen.getByTestId('button')
-    expect(button).toBeInTheDocument();
-    expect(button.tagName).toBe('BUTTON');
-});
